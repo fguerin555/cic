0b2cc0e750211e49a8a71b61377c506bc02cf2f9
FredericGuerin <fguerin555@gmail.com>
2025-06-14 12:16:03 +0200

    modifs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "./Global.css";
 import App from "./App";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
