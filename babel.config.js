98d48c02d88bec24e883bbb724a6af54aaecba83
FredericGuerin <fguerin555@gmail.com>
2025-06-14 16:26:35 +0200

    Updates

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry",
+        corejs: "^3.43.0",
+      },
+    ],
+    "@babel/preset-react",
+  ],
+};
