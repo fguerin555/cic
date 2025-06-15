619d949e1e8e27a3dd59c27d737f0bc0580f262d
FredericGuerin <fguerin555@gmail.com>
2025-06-15 12:14:20 +0200

    dreniereTentative

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "./Global.css";
 import App from "./App";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
